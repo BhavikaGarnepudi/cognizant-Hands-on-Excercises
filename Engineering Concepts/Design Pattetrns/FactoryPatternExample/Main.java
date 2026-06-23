cc7f2f6c0ee5458d943ceac695eaf8a8bacc168f
BhavikaGarnepudi <g.bhavika2005@gmail.com>
2026-06-23 16:35:59 +0530

    Add Factory Method Pattern example

---
+++ Engineering Concepts/Design Pattetrns/FactoryPatternExample/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+        DocumentFactory factory;
+        Document document;
+
+        // Word
+        factory = new WordDocumentFactory();
+        document = factory.createDocument();
+        document.open();
+        document.save();
+        document.close();
+
+        System.out.println("---");
+
+        // PDF
+        factory = new PdfDocumentFactory();
+        document = factory.createDocument();
+        document.open();
+        document.save();
+        document.close();
+
+        System.out.println("---");
+
+        // Excel
+        factory = new ExcelDocumentFactory();
+        document = factory.createDocument();
+        document.open();
+        document.save();
+        document.close();
+    }
+}
